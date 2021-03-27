953f4297761d679d6ea2d07a9a73391b41ab7d53
mateusz <kucka.mateusz14@gmail.com>
2021-03-27 21:22:34 +0100

    move login and signup tests to tests/auth dir

--- src/test/java/invi/tests/auth/SignUpTest.java
+++ src/test/java/invi/tests/auth/SignUpTest.java
@@ -1,11 +1,11 @@
-package invi.tests;
+package invi.tests.auth;
 
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import invi.driver.DeviceManager;
 import invi.listeners.TestListener;
 import invi.pages.guest.MainPage;
 import invi.pages.open.LandingPage;
-import invi.pages.open.SignInPage;
+import invi.pages.open.SignUpPage;
 import io.appium.java_client.MobileDriver;
 import io.appium.java_client.MobileElement;
 import org.testng.Assert;
@@ -13,16 +13,16 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
 
 @Listeners({
         ExtentITestListenerClassAdapter.class,
         TestListener.class
 })
-public class LogInTest {
-    private final static String CORRECT_EMAIL = "test.existing@email.com";
+public class SignUpTest {
+    private final static String CORRECT_EMAIL = "test." + String.valueOf(new Date().getTime()) + "@email.com";
     private final static String INCORRECT_EMAIL = "incorrect.email";
     private final static String INCORRECT_PASSWORD = "zaqwsx";
     private final static String EMPTY = "";
@@ -35,7 +35,7 @@ public class LogInTest {
 
     @BeforeMethod
     public void setUp() {
-        Map<String, String> params = new HashMap<>();
+        Map <String, String> params = new HashMap<>();
         params.put("packageName", "com.kiksoft.invi");
         params.put("activity", "splash.SplashActivity");
 
@@ -45,20 +45,20 @@ public class LogInTest {
     }
 
     @Test
-    public void logInTest() {
-        LandingPage landingPage = new LandingPage(this.driver);
-        SignInPage signInPage = new SignInPage(this.driver);
-        MainPage mainPage = new MainPage(this.driver);
+    public void signUpTest() {
+        LandingPage landingPage = new LandingPage(driver);
+        SignUpPage signUpPage = new SignUpPage(driver);
+        MainPage mainPage = new MainPage(driver);
 
-        landingPage.selectSignIn();
-        signInPage.signIn(EMPTY, INCORRECT_PASSWORD);
-        Assert.assertEquals(EMPTY_FIELD_ERROR_MESSAGE, signInPage.getEmailInputErrorLabel().getText());
 
-        signInPage.signIn(INCORRECT_EMAIL, INCORRECT_PASSWORD);
-        Assert.assertEquals(EMAIL_FORMAT_ERROR_MESSAGE, signInPage.getEmailInputErrorLabel().getText());
+        landingPage.selectSignUp();
+        signUpPage.signUp(EMPTY, INCORRECT_PASSWORD);
+        Assert.assertEquals(EMPTY_FIELD_ERROR_MESSAGE, signUpPage.getErrorInputLabel().getText());
 
-        signInPage.signIn(CORRECT_EMAIL, CORRECT_PASSWORD);
-        Assert.assertTrue(mainPage.getLogOutButton().isDisplayed());
+        signUpPage.signUp(INCORRECT_EMAIL, INCORRECT_PASSWORD);
+        Assert.assertEquals(EMAIL_FORMAT_ERROR_MESSAGE, signUpPage.getErrorInputLabel().getText());
 
+        signUpPage.signUp(CORRECT_EMAIL, CORRECT_PASSWORD);
+        Assert.assertNotNull(mainPage.getLogOutButton());
     }
 }
