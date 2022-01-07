b2248b18c5199a50e9b9e5608208448c658b1980
mateusz <kucka.mateusz14@gmail.com>
2022-01-07 18:56:11 +0100

    update local device capabilities

---
+++ src/test/java/invi/capabilities/EmulatorLocal.java
@@ -0,0 +1,30 @@
+package invi.capabilities;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+
+public class EmulatorLocal implements Device{
+    private DesiredCapabilities dc = new DesiredCapabilities();
+
+    public EmulatorLocal() {
+        File file = new File(Device.APK_PATH);
+        String apkPath = file.getAbsoluteFile().getPath();
+
+        dc.setCapability("platformName", "Android");
+        dc.setCapability("platformVersion", "11");
+        dc.setCapability("appActivity", ".splash.SplashActivity");
+        dc.setCapability("app", apkPath);
+        dc.setCapability("appPackage", "com.kiksoft.invi");
+        dc.setCapability("deviceName", "Pixel 3 API 30");
+        dc.setCapability("allowTestPackages", "true");
+        dc.setCapability("automationName", "UiAutomator2");
+        dc.setCapability("appWaitActivity", ".splash.SplashActivity, com.kiksoft.auth.welcome.WelcomeActivity");
+        dc.setCapability("appWaitPackage", "com.kiksoft.invi, ");
+        dc.setCapability("appWaitDuration", "50000");
+    }
+
+    public DesiredCapabilities getDc() {
+        return dc;
+    }
+}
