296d0177719e5434ad8e77dc5956a5c1fc50ff14
mateusz <kucka.mateusz14@gmail.com>
2022-04-17 14:24:52 +0200

    add aws execution (App.java) via mvn

---
+++ src/main/java/invi/exceptions/InvalidTestRunArgException.java
@@ -0,0 +1,7 @@
+package invi.exceptions;
+
+public class InvalidTestRunArgException extends RuntimeException {
+    public InvalidTestRunArgException(String message) {
+        super(message);
+    }
+}
