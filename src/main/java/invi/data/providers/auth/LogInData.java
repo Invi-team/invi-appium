f6e42d0e5be56681cfd6ea1a31e59c2a5d029e74
mateusz <kucka.mateusz14@gmail.com>
2022-07-26 16:29:47 +0200

    reimplement services

--- src/main/java/invi/data/providers/auth/LogInData.java
+++ src/main/java/invi/data/providers/auth/LogInData.java
@@ -1,12 +1,9 @@
 package invi.data.providers.auth;
 
-import invi.beans.Register;
 import invi.data.providers.TestDataProvider;
-import invi.http.RegisterCaller;
-import okhttp3.Response;
+import invi.services.AuthService;
 import org.testng.annotations.DataProvider;
 
-import java.io.IOException;
 import java.util.logging.Logger;
 
 public class LogInData extends TestDataProvider {
@@ -20,26 +17,9 @@ public class LogInData extends TestDataProvider {
         String email = String.format("appium%s@gmail.com", currentDate);
         String password = propertiesHandler.getProperty("config.properties", "invi.accounts.password");
         String deviceId = String.format("device%s", currentDate);
+        AuthService authService = new AuthService();
 
-        RegisterCaller registerCaller = new RegisterCaller();
-        Response registerResponse = null;
-        Register registerBean = Register.builder()
-                .deviceId(deviceId)
-                .email(email)
-                .password(password)
-                .build();
-
-        try {
-            registerResponse = registerCaller.call(registerBean);
-        } catch (IOException e) {
-            e.printStackTrace();
-            LOGGER.throwing(LogInData.class.getName(), "provideData", e);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            LOGGER.throwing(LogInData.class.getName(), "provideData", e);
-        }
-
-        LOGGER.info(LogInData.class.getName() + " user register status code: " + registerResponse.code());
+        authService.register(email, password, deviceId);
 
         return new Object[][]{new Object[]{email, password, incorrectEmail, incorrectPassword}};
     }
